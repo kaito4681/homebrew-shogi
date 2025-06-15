a133cac186cc34b4146f86af21d5b441a1182b02
kaito4681 <4681kaito@gmail.com>
2025-06-16 05:38:19 +0900

    add: shogi-home

---
+++ Casks/shogihome.rb
@@ -0,0 +1,16 @@
+cask "shogihome" do
+  homepage "https://github.com/sunfish-shogi/shogihome"
+  name "ShogiHome"
+  version "1.23.0"
+  sha256 :no_check
+  url "https://github.com/sunfish-shogi/shogihome/releases/download/v1.23.0/release-v1.23.0-mac.zip"
+
+  container nested: "ShogiHome-1.23.0-universal.dmg"
+
+  app "ShogiHome.app"
+
+  postflight do
+    #「開発元が未確認のため開けません」というダイアログを抑制
+    system_command "/usr/bin/xattr", args: ["-rd", "com.apple.quarantine", "#{staged_path}/ShogiHome.app"], sudo: true
+  end
+end
---
+++ README.md
@@ -0,0 +1,6 @@
+# ShogiHome を homebrew で入れる。
+
+```bash
+brew tap kaito4681/shogi
+brew install shogihome
+```
